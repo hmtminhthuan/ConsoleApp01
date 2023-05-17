f38ff7bf32fbeaebbdd48a3613f0e176c5f78901
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-05-17 08:50:31 +0700

    upload

--- ConsoleApp01/Program.cs
+++ ConsoleApp01/Program.cs
@@ -8,6 +8,9 @@ namespace ConsoleApp01
         {
             int a = 10, b = 20, c = 30; double d = 12.5;
             Console.WriteLine($"Sum = {Add(a,b,d)}");
+            Console.WriteLine($"Sum of Serials = {sumOfSerials(a)}");
+            Console.WriteLine($"Sum of Serials By Loop = {sumOfSerialsByLoop(a)}");
+            Console.WriteLine($"Sum of Fibonacci = {findFibonacci(8)}");
             Console.ReadLine();
         }
         // $" ": string interpolation
@@ -17,9 +20,36 @@ namespace ConsoleApp01
         // overloading: nạp chồng/quá tải (cùng tên hàm, có thể khác kiểu dữ liệu, khác số lượng tham số)
         public static double Add(double x, double y) => x + y;
         public static double Add(int x, int y, double z) => Add(Add(x,y),z);
-        
-            
-        
-            
+
+        /*
+         Required: Sum of serials: 1+2...+n
+        + loop
+        + recursion
+        */
+        public static long sumOfSerials(int n)
+        {
+            if (n <= 0) return 0;
+            return n%2==0 ? (sumOfSerials(n-1)+n) : sumOfSerials(n-1);
+        }
+
+        public static long sumOfSerialsByLoop(int n)
+        {
+            long total = 0;
+            int i = 0;
+            // while, do while, for, foreach
+            while (i <= n) { 
+                if(i%2==0) total += i; 
+                i++; 
+                // toan tu 1 ngoi: unary : +=, ++
+            }
+            return total;
+        }
+
+        public static long findFibonacci(int n)
+        {
+            if (n <= 1) return 1;
+            return findFibonacci(n-1)+findFibonacci(n-2);
+        }
+
     }
 }
