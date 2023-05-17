ce63b75e7029d992f56716dc9a254e4d45ff3ce6
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-05-17 08:08:12 +0700

    upload

--- ConsoleApp01/Program.cs
+++ ConsoleApp01/Program.cs
@@ -2,11 +2,24 @@
 
 namespace ConsoleApp01
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 10, b = 20, c = 30; double d = 12.5;
+            Console.WriteLine($"Sum = {Add(a,b,d)}");
+            Console.ReadLine();
         }
+        // $" ": string interpolation
+
+        public static int Add(int x, int y) => x + y;
+        // int default = int32
+        // overloading: nạp chồng/quá tải (cùng tên hàm, có thể khác kiểu dữ liệu, khác số lượng tham số)
+        public static double Add(double x, double y) => x + y;
+        public static double Add(int x, int y, double z) => Add(Add(x,y),z);
+        
+            
+        
+            
     }
 }
